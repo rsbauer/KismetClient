d2d90beb20ee3ab7e31a16aa7ca9a95610a54237
Astro <astro@Astros-MacBook-Pro.local>
2011-07-16 09:56:36 -0400

    Initial Commit

---
+++ KismetClient/KismetClientAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  KismetClientAppDelegate.h
+//  KismetClient
+//
+//  Created by Astro on 7/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KismetClientAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
